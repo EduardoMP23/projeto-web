ee55f3e91b2230ad064d688f3ce725a1e02104d3
Eduardo De Mello Pereira <eduardo-MP@rede.ulbra.br>
2024-11-07 21:38:36 -0300

    envio do projeto

---
+++ resources/views/pokemon/index.blade.php
@@ -0,0 +1,12 @@
+@foreach($pokemon as $pokemon)
+    <div>
+        <h3>{{ $pokemon->name }}</h3>
+        <p>{{ $pokemon->type }}</p>
+        <a href="{{ url('pokemon/'.$pokemon->id.'/edit') }}">Edit</a>
+        <form action="{{ url('pokemon/'.$pokemon->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </div>
+@endforeach
